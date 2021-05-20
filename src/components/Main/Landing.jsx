6c85077e373bd43aef54a196bab2f55080d8195c
Rahul Lanjewar <rahullanjewar93@gmail.com>
2021-05-20 22:45:04 +0530

    feat(landing) add content to landing pages

--- src/components/Main/Landing.jsx
+++ src/components/Main/Landing.jsx
@@ -1,11 +1,11 @@
-import { Grid, Typography, makeStyles } from '@material-ui/core'
+import { Grid, Typography, makeStyles, Button, Box } from '@material-ui/core'
 // import BannerImage from '../../assets/images/homebackground.jpg'
 import React from 'react'
 
 const useStyles = makeStyles({
     banner: {
         minHeight: '100vh',
-        display: 'flex'
+        display: 'flex',
     },
     bannerInfo: {
         backgroundColor: '#faf6f2'
@@ -16,22 +16,57 @@ const useStyles = makeStyles({
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
         backgroundSize: 'cover'
+    },
+    containerLeft: {
+        position: 'relative',
+        top: '50%',
+        left: '40%',
+        width: '50%',
+        transform: 'translate(-50%,-50%)'
+    },
+    containerRight: {
+        position: 'relative',
+        top: '50%',
+        left: '50%',
+        width: '50%',
+        transform: 'translate(-50%,-50%)'
+    },
+    cite: {
+        color: 'white'
+    },
+    button: {
+        textTransform: 'capitalize'
+    },
+    text: {
+        margin: '20px 0px'
     }
 })
 
 const Landing = () => {
     const classes = useStyles()
     return (
-        <div>
-            <Grid className={classes.banner} container>
-                <Grid className={classes.bannerInfo} item xs={12} sm={8}>
-                    <Typography>Algo Visualizer</Typography>
-                </Grid>
-                <Grid className={classes.bannerHero} item xs={12} sm={4}>
-                    <Typography>Hi</Typography>
-                </Grid>
+        <Grid className={classes.banner} container>
+            <Grid className={classes.bannerInfo} item xs={12} sm={8}>
+                <Box className={classes.containerLeft} >
+                    <Typography variant='h2' className={classes.text}>
+                        Algo Visualizer
+                    </Typography>
+                    <Typography variant='h4' className={classes.text}>
+                        <em>Your best mate to solve your algorithms</em>
+                    </Typography>
+                    <Button className={classes.button} variant='outlined' >
+                        Get Started
+                    </Button>
+                </Box>
             </Grid>
-        </div>
+            <Grid className={classes.bannerHero} item xs={12} sm={4}>
+                <Box className={classes.containerRight} >
+                    <Typography variant='h4'>
+                        <cite className={classes.cite}>"A picture is worth of thousands words"</cite>
+                    </Typography>
+                </Box>
+            </Grid>
+        </Grid>
     )
 }
 
