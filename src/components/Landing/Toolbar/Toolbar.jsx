c9f1b4d74d6ea5252d514776bc4db049b6406937
Aashka Doshi <aashkadoshi7@gmail.com>
2021-10-18 17:17:09 +0530

    removed material ui

--- src/components/Landing/Toolbar/Toolbar.jsx
+++ src/components/Landing/Toolbar/Toolbar.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Divider, Grid, Paper, Typography } from '@material-ui/core'
+import { Box, Button, Container, Divider, Grid, Paper, Typography } from '@mui/material'
 import React from 'react'
 import SingleButton from './SingleButton'
 
